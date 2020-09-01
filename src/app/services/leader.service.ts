6284946b1ffa85c107f2d10a6a42462439c4ec2a
handeoz <handeozturk1114@gmail.com>
2020-09-01 09:11:38 +0300

    Assignment 2

---
+++ src/app/services/leader.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Leader} from '../shared/leader';
+import { LEADERS} from '../shared/leaders';
+import {Dish} from '../shared/dish';
+import {DISHES} from '../shared/dishes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderService {
+
+  constructor() { }
+
+  getLeaders(): Leader[] {
+    return LEADERS;
+  }
+
+  getLeader(id: string): Leader {
+    return LEADERS.filter((leader) => (leader.id === id)) [0];
+  }
+
+  getFeaturedLeader(): Leader {
+    return LEADERS.filter((leader) => leader.featured) [0];
+  }
+}
